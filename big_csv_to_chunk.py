7f00b2b49a113fac02e897dd39baceb5493d97be
pankajrajput1462 <pankajrajput1462@users.noreply.github.com>
2020-07-16 13:16:19 +0200

    Update big_csv_to_chunk.py

--- big_csv_to_chunk.py
+++ big_csv_to_chunk.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import date
 
 fileUrl = 'resource\main.csv'
 chunkFileName = 'Chunk_'
@@ -20,4 +21,16 @@ for chunk in pd.read_csv(fileUrl, chunksize=100, sep='|'):
                               columns=['Header_1', 'Header_2', 'Header_3', 'Header_4', 'Header_5', 'Header_6',
                                        'Header_7'])
     data_frame.to_csv(chunkFileName + str(count) + '.csv', index=None, sep='|')
+    count += 1
+
+    
+   
+date_today = str(date.today()).replace('-', '') + '_'
+
+for chunk in pd.read_csv(fileUrl, chunksize=10000, sep='|'):
+    print(chunk.shape)
+    data_frame = pd.DataFrame(chunk,
+                              columns=['Header', 'Header', 'Header', 'Header', 'Header', 'Header',
+                                       'Header'])
+    data_frame.to_csv(chunkFileName + date_today + str(count) + '.csv', index=None, sep='|')
     count += 1
