25711674024b23ac4240b9d82250718a0103fa1a
pankajrajput1462 <pankajrajput1462@users.noreply.github.com>
2020-07-16 13:02:04 +0200

    Update big_csv_to_chunk.py

--- big_csv_to_chunk.py
+++ big_csv_to_chunk.py
@@ -10,3 +10,14 @@ for chunk in pd.read_csv(fileUrl, chunksize=500):
     data_frame = pd.DataFrame(chunk, columns=['Header_1', 'Header_2', 'Header_3', 'Header_4'])
     data_frame.to_csv(chunkFileName + str(count) + '.csv', index=None)
     count += 1
+
+
+    
+# with specific seperator
+for chunk in pd.read_csv(fileUrl, chunksize=100, sep='|'):
+    print(chunk.shape)
+    data_frame = pd.DataFrame(chunk,
+                              columns=['Header_1', 'Header_2', 'Header_3', 'Header_4', 'Header_5', 'Header_6',
+                                       'Header_7'])
+    data_frame.to_csv(chunkFileName + str(count) + '.csv', index=None, sep='|')
+    count += 1
